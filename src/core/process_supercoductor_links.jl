2d146025fc188b6413900f1d884ac9556e3ce670
RicardoG <117919292+rgallo23@users.noreply.github.com>
2024-04-16 10:51:54 +0200

    update in process_sc_links function

--- src/core/process_supercoductor_links.jl
+++ src/core/process_supercoductor_links.jl
@@ -5,8 +5,12 @@
 # Additionally, the cooling power is added at both sides of the converters
 # in their associated AC nodes, half at each end
 
-function process_superconductor_links!(data::Dict{String,Any})
-    
+function process_superconductor_links!(data::Dict{String,Any},sc_data::Dict{String,Any})
+    # Extract dc buses of converter and join them
+    node_dc = sc_data["1"]["fbusdc"]
+    for 
+    # Calculate the losses and add them to the ac buses of converter
+    # for loop to eliminate the branches
 
 end
 
@@ -15,6 +19,7 @@ function add_sc_links(data::Dict{String,Any},sc_links::Vector{String})
     for sc_link in sc_links
         if haskey(data["branchdc"],sc_link)
             sc_data[sc_link] = deepcopy(data["branchdc"][sc_link])
+            sc_data[sc_link]["length"] = 200 # All sc branches 200 km, to modify later
         end
     end
     return sc_data
@@ -22,8 +27,6 @@ end
 
 # This function calculate the cooling losses based on length
 function cooling_losses(length)
-    p_losses = 500 # Losses equal to 500 kW per station and 1 station every 25 km
-    P_cooling = (length/25)*p_losses
-
-    return P_cooling
+    p_losses = 0.005 # Losses equal to 500 kW per station and 1 station every 25 km
+    return ceil((length/25))*p_losses
 end
