d86f4fc7bd6f81a9acba22383911b03c2989b7bc
RicardoG <117919292+rgallo23@users.noreply.github.com>
2024-04-25 15:44:16 +0200

    update in cooling_losses

--- test/scripts/SC_test.jl
+++ test/scripts/SC_test.jl
@@ -4,7 +4,7 @@
 
 using PowerModels ; const _PM = PowerModels
 using PowerModelsACDC ; const _PMACDC = PowerModelsACDC
-#using Gurobi
+using Gurobi
 using Ipopt
 
 # Include new Functions
@@ -18,10 +18,10 @@ data_original = deepcopy(data)
 nl_solver = Ipopt.Optimizer
 
 # Define superconductor links
-sc_links = ["1"] # Vector to state which dc branches are superconductor links
+sc_links = ["1"] # Vector to state whihc dc branches are superconductor links
 sc_data = Dict{String,Any}() # Dict to save sc branches data
 
-sc_data = add_sc_links(data,sc_links)
+sc_data = add_sc_links!(data,sc_links)
 
 process_superconductor_links!(data,sc_data)
 
@@ -31,28 +31,23 @@ _PMACDC.process_additional_data!(data)
 _PMACDC.process_additional_data!(data_original)
 result = _PMACDC.run_acdcopf(data,ACPPowerModel,nl_solver;setting = s)
 result_original = _PMACDC.run_acdcopf(data_original,ACPPowerModel,nl_solver;setting = s)
-result_2 = _PMACDC.run_acdcopf(data,ACPPowerModel,nl_solver;setting = s)
+#result_2 = _PMACDC.run_acdcopf(data,ACPPowerModel,nl_solver;setting = s)
 
 print("-------------------------------------------------------\n")
 print("                  Optimization Results                 \n")
 print("-------------------------------------------------------\n")
 print("\n Case 1: No SC Branches : ",result_original["objective"], "\n")
 print("\n Case 2: 1  SC Branch   : ",result["objective"], "\n")
-print("\n Case 3: 2  SC Branches : ",result_2["objective"], "\n")
+#print("\n Case 3: 2  SC Branches : ",result_2["objective"], "\n")
 
 ##### Results ######
 # case5_acdc    = 194.139
 # case5_acdc_sc = 196.496  # No SC branches
 # case5_acdc_sc = 202.236 # 1 SC branch
 
-using CSV
-
-data_list = []
-
-for (conv_id,convdc) in result["solution"]["convdc"]
-    for (param,value) in convdc
-        push!(data_list, (conv_id, param, value))
-    end
+for (conv_id,conv_dc) in data["convdc"]
+    print("Converter: ","$conv_id"," dc bus is: ",conv_dc["busdc_i"],"\n")
 end
 
-CSV.write("data.csv", data_list, header=["Entry","Parameter","value"])
+length = 200
+Losses = cooling_losses(length)[1]
