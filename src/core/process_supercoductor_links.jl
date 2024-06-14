fa91e9b393483adc46f35df83dbc91f295714ae6
RicardoG <117919292+rgallo23@users.noreply.github.com>
2024-06-14 15:21:55 +0200

    Update 14/06

--- src/core/process_supercoductor_links.jl
+++ src/core/process_supercoductor_links.jl
@@ -7,42 +7,35 @@
 
 function process_superconductor_links!(data::Dict{String,Any},sc_data::Dict{String,Any})
     # Extract dc buses of converter, join them
+    for (conv_id, conv_dc) in sc_data
+        for (branch_id, branch_dc) in data
+            if conv_dc["busdc_i"] == branch_dc["tbusdc"]
+                conv_dc["busdc_i"] = branch_dc["fbusdc"]
+            end
+        end
+        
+        # Check if this works with the for loops
+        if any(load["load_bus"] == conv_dc["busac_i"] for (load_id, load) in data["load"])
+            for (load_id, load) in data["load"]
+                if load["load_bus"] == conv_dc["busac_i"] # Add aux load from cooling
+                    load["pd"] = load["pd"] + branch_dc["p_aux"]/2
+                    load["qd"] = load["qd"] + branch_dc["q_aux"]/2
+                end
+            end
+        else
+            load_id_new = length(data["load"]) + 1
+            data["load"][string(load_id_new)] = Dict("source_id" => ["bus", conv_dc["busac_i"]],
+                                                     "load_bus" => conv_dc["busac_i"],
+                                                     "status" => 1, 
+                                                     "qd" => branch_dc["q_aux"]/2, 
+                                                     "pd" => branch_dc["p_aux"]/2,
+                                                     "index" => load_id_new)
+        end    
+    end    
 
     for (branch_id,branch_dc) in sc_data
-        for (conv_id,conv_dc) in data["convdc"]
-            if conv_dc["busdc_i"] == branch_dc["tbusdc"] #|| conv_dc["busdc_i"] == branch_dc["fbusdc"]
-                conv_dc["busdc_i"] = branch_dc["fbusdc"] # Connect converter at both sides of sc branch to same dc busdc_i
-            end
-        end
-
         delete!(data["branchdc"],"$branch_id")
-
-        length(collect(keys(data["load"])))
-
-        
-
-        for (load_id,load) in data["load"]
-            if haskey(load,"load_bus")
-                #print("AC bus is",load["load_bus"])
-            end
-        end
-        # for (load_id,load) in data["load"] # Check with length of dict
-        # # Create a new entry for the data["load"] dictionary
-        #     if load["load_bus"] == conv_dc["busac_i"]
-        #         load["pd"] = load["pd"] + branch_dc["p_aux"]/2 # Adds half the aux losses at each side of the converter
-        #         load["qd"] = load["qd"] + branch_dc["q_aux"]/2
-        #     end
-        #     if 
-
-        # end
     end
-
-    if haskey() # Check if there is a load connected to the converter ac bus
-        if load["load_bus"] == conv_dc["busac_i"] # Update later to consider no previous load
-            load["pd"] = load["pd"] + branch_dc["p_aux"]/2 # Adds half the losses at each side of the converter
-            load["qd"] = load["qd"] + branch_dc["q_aux"]/2
-        end
-    else 
 end
 
 # Function to add sc key to branchdc data
@@ -54,8 +47,8 @@ function add_sc_links!(data::Dict{String,Any},sc_links::Vector{String})
         if haskey(data["branchdc"],sc_link)
             sc_data[sc_link] = deepcopy(data["branchdc"][sc_link])
             sc_data[sc_link]["length"] = 200 # All sc branches 200 km, to modify later
-            sc_data[sc_link]["p_loss"] = cooling_losses(sc_data[sc_link]["length"])[1]
-            sc_data[sc_link]["q_loss"] = cooling_losses(sc_data[sc_link]["length"])[2]
+            sc_data[sc_link]["p_aux"] = cooling_losses(sc_data[sc_link]["length"])[1]
+            sc_data[sc_link]["q_aux"] = cooling_losses(sc_data[sc_link]["length"])[2]
             sc_data[sc_link]["sc"]     = true
         end
     end
@@ -69,8 +62,8 @@ function add_sc_links_2!(data::Dict{String,Any},sc_links::Vector{String})
         if haskey(data["branchdc"],sc_link)
             sc_data[sc_link] = deepcopy(data["branchdc"][sc_link])
             sc_data[sc_link]["length"] = 200 # All sc branches 200 km, to modify later
-            sc_data[sc_link]["p_loss"] = cooling_losses(sc_data[sc_link]["length"])[1]
-            sc_data[sc_link]["q_loss"] = cooling_losses(sc_data[sc_link]["length"])[2]
+            sc_data[sc_link]["p_aux"] = cooling_losses(sc_data[sc_link]["length"])[1]
+            sc_data[sc_link]["q_aux"] = cooling_losses(sc_data[sc_link]["length"])[2]
         end
     end
     merge!(data["branchdc"],sc_data)
@@ -80,10 +73,47 @@ end
 function cooling_losses(length)
     p_losses = 0.005 # Losses equal to 500 kW per station and 1 station every 25 km
     pf = 0.85
-    aux_losses = []
+    aux_power = []
     p_aux = ceil((length/25))*p_losses
     q_aux = round(p_load*tan(acos(pf)),digits=3)
-    push!(losses,p_aux) # Active power losses
-    push!(losses,q_aux) # Reactive power losses
-    return aux_losses # Vector with [p_aux,q_aux]
+    push!(aux_power,p_aux) # Active power losses
+    push!(aux_power,q_aux) # Reactive power losses
+    return aux_power # Vector with [p_aux,q_aux]
 end
+
+# for (branch_id,branch_dc) in sc_data
+#     for (conv_id,conv_dc) in data["convdc"]
+#         if conv_dc["busdc_i"] == branch_dc["tbusdc"] #|| conv_dc["busdc_i"] == branch_dc["fbusdc"]
+#             conv_dc["busdc_i"] = branch_dc["fbusdc"] # Connect converter at both sides of sc branch to same dc busdc_i
+#         end
+#     end
+
+#     delete!(data["branchdc"],"$branch_id")
+
+#     length(collect(keys(data["load"])))
+
+    
+
+#     for (load_id,load) in data["load"]
+#         if haskey(load,"load_bus")
+#             #print("AC bus is",load["load_bus"])
+#         end
+#     end
+#     # for (load_id,load) in data["load"] # Check with length of dict
+#     # # Create a new entry for the data["load"] dictionary
+#     #     if load["load_bus"] == conv_dc["busac_i"]
+#     #         load["pd"] = load["pd"] + branch_dc["p_aux"]/2 # Adds half the aux losses at each side of the converter
+#     #         load["qd"] = load["qd"] + branch_dc["q_aux"]/2
+#     #     end
+#     #     if 
+
+#     # end
+# end
+
+# if haskey() # Check if there is a load connected to the converter ac bus
+#     if load["load_bus"] == conv_dc["busac_i"] # Update later to consider no previous load
+#         load["pd"] = load["pd"] + branch_dc["p_aux"]/2 # Adds half the losses at each side of the converter
+#         load["qd"] = load["qd"] + branch_dc["q_aux"]/2
+#     end
+# else 
+# end
