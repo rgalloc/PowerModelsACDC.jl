b07e1ffc41c83a0d288d105b33cf1387f0050a75
rgallo23 <117919292+rgallo23@users.noreply.github.com>
2024-07-05 14:10:39 +0200

    Update process_supercoductor_links.jl

--- src/core/process_supercoductor_links.jl
+++ src/core/process_supercoductor_links.jl
@@ -38,9 +38,52 @@ function process_superconductor_links!(data::Dict{String,Any},sc_data::Dict{Stri
     end
 end
 
+function process_superconductor_links2!(data::Dict{String,Any})
+    
+    for (conv_id, conv_dc) in data["convdc"]        
+        for (branch_id, branch_dc) in data["branchdc"]
+            if branch_dc["sc"] == true
+                if conv_dc["busdc_i"] == branch_dc["tbusdc"] || conv_dc["busdc_i"] == branch_dc["fbusdc"]
+                    conv_dc["busdc_i"] = branch_dc["fbusdc"]
+                    conv_dc["p_aux"] = branch_dc["p_aux"]/2
+                    conv_dc["q_aux"] = branch_dc["q_aux"]/2
+                    conv_dc["sc"]    =  true
+                end                
+            end
+        end
+        
+        if haskey(conv_dc,"sc")
+            if any(load["load_bus"] == conv_dc["busac_i"] for (load_id, load) in data["load"])
+                for (load_id, load) in data["load"]
+                    if load["load_bus"] == conv_dc["busac_i"] # Add aux load from cooling
+                        load["pd"] = load["pd"] + conv_dc["p_aux"]
+                        load["qd"] = load["qd"] + conv_dc["q_aux"]
+                    end
+                end
+            else
+                load_id_new = length(data["load"]) + 1
+                data["load"][string(load_id_new)] = Dict("source_id" => ["bus", conv_dc["busac_i"]],
+                                                        "load_bus" => conv_dc["busac_i"],
+                                                        "status" => 1, 
+                                                        "qd" => conv_dc["p_aux"], 
+                                                        "pd" => conv_dc["q_aux"],
+                                                        "index" => load_id_new)
+            end  
+        end
+    end
+
+    for (branch_id,branch_dc) in data["branchdc"]
+        if branch_dc["sc"] == true
+            delete!(data["branchdc"],"$branch_id")
+        end
+    end
+
+end
+
 # Function to add sc key to branchdc data
 # Creates sc_data dictionary with superconducting branches data
 # Merges sc_data with the grid data
+
 # function add_sc_links!(data::Dict{String,Any},sc_links::Vector{String})
     
 #     sc_data = Dict{String, Dict{String, Any}}() # Empty dict to save sc data
@@ -89,48 +132,6 @@ function add_sc_links_3!(data::Dict{String,Any},sc_links::Vector{String})
             branch_dc["sc"] = false
         end
     end  
-end
-
-function process_superconductor_links2!(data::Dict{String,Any})
-    
-    for (conv_id, conv_dc) in data["convdc"]        
-        for (branch_id, branch_dc) in data["branchdc"]
-            if branch_dc["sc"] == true
-                if conv_dc["busdc_i"] == branch_dc["tbusdc"] || conv_dc["busdc_i"] == branch_dc["fbusdc"]
-                    conv_dc["busdc_i"] = branch_dc["fbusdc"]
-                    conv_dc["p_aux"] = branch_dc["p_aux"]/2
-                    conv_dc["q_aux"] = branch_dc["q_aux"]/2
-                    conv_dc["sc"]    =  true
-                end                
-            end
-        end
-        
-        if haskey(conv_dc,"sc")
-            if any(load["load_bus"] == conv_dc["busac_i"] for (load_id, load) in data["load"])
-                for (load_id, load) in data["load"]
-                    if load["load_bus"] == conv_dc["busac_i"] # Add aux load from cooling
-                        load["pd"] = load["pd"] + conv_dc["p_aux"]
-                        load["qd"] = load["qd"] + conv_dc["q_aux"]
-                    end
-                end
-            else
-                load_id_new = length(data["load"]) + 1
-                data["load"][string(load_id_new)] = Dict("source_id" => ["bus", conv_dc["busac_i"]],
-                                                        "load_bus" => conv_dc["busac_i"],
-                                                        "status" => 1, 
-                                                        "qd" => conv_dc["p_aux"], 
-                                                        "pd" => conv_dc["q_aux"],
-                                                        "index" => load_id_new)
-            end  
-        end
-    end
-
-    for (branch_id,branch_dc) in data["branchdc"]
-        if branch_dc["sc"] == true
-            delete!(data["branchdc"],"$branch_id")
-        end
-    end
-
 end
 
 # This function calculate the cooling losses based on length
