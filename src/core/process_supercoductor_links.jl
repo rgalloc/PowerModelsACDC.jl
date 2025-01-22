6de91062f34c33a15f96ee71dc2bb652c8e047a7
Ricardo Gallo <ricardoandres.gallocaicedo@kuleuven.be>
2025-01-22 15:10:29 +0100

    Clean old functions

--- src/core/process_supercoductor_links.jl
+++ src/core/process_supercoductor_links.jl
@@ -5,39 +5,40 @@
 # Additionally, the cooling power is added at both sides of the converters
 # in their associated AC nodes, half at each end
 
-function process_superconductor_links!(data::Dict{String,Any},sc_data::Dict{String,Any})
-    # Extract dc buses of converter, join them
-    for (conv_id, conv_dc) in data["convdc"]
-        for (branch_id, branch_dc) in sc_data
-            if conv_dc["busdc_i"] == branch_dc["tbusdc"]
-                conv_dc["busdc_i"] = branch_dc["fbusdc"]
-            end
-        end
+# function process_superconductor_links!(data::Dict{String,Any},sc_data::Dict{String,Any})
+#     # Extract dc buses of converter, join them
+#     for (conv_id, conv_dc) in data["convdc"]
+#         for (branch_id, branch_dc) in sc_data
+#             if conv_dc["busdc_i"] == branch_dc["tbusdc"]
+#                 conv_dc["busdc_i"] = branch_dc["fbusdc"]
+#             end
+#         end
         
-        # Check if this works with the for loops
-        if any(load["load_bus"] == conv_dc["busac_i"] for (load_id, load) in data["load"])
-            for (load_id, load) in data["load"]
-                if load["load_bus"] == conv_dc["busac_i"] # Add aux load from cooling
-                    load["pd"] = load["pd"] + (branch_dc["p_aux"]/2)*0.1
-                    load["qd"] = load["qd"] + (branch_dc["q_aux"]/2)*0.1
-                end
-            end
-        else
-            load_id_new = length(data["load"]) + 1
-            data["load"][string(load_id_new)] = Dict("source_id" => ["bus", conv_dc["busac_i"]],
-                                                     "load_bus" => conv_dc["busac_i"],
-                                                     "status" => 1, 
-                                                     "qd" => branch_dc["q_aux"]/2, 
-                                                     "pd" => branch_dc["p_aux"]/2,
-                                                     "index" => load_id_new)
-        end    
-    end    
+#         # Check if this works with the for loops
+#         if any(load["load_bus"] == conv_dc["busac_i"] for (load_id, load) in data["load"])
+#             for (load_id, load) in data["load"]
+#                 if load["load_bus"] == conv_dc["busac_i"] # Add aux load from cooling
+#                     load["pd"] = load["pd"] + (branch_dc["p_aux"]/2)*0.1
+#                     load["qd"] = load["qd"] + (branch_dc["q_aux"]/2)*0.1
+#                 end
+#             end
+#         else
+#             load_id_new = length(data["load"]) + 1
+#             data["load"][string(load_id_new)] = Dict("source_id" => ["bus", conv_dc["busac_i"]],
+#                                                      "load_bus" => conv_dc["busac_i"],
+#                                                      "status" => 1, 
+#                                                      "qd" => branch_dc["q_aux"]/2, 
+#                                                      "pd" => branch_dc["p_aux"]/2,
+#                                                      "index" => load_id_new)
+#         end    
+#     end    
 
-    for (branch_id,branch_dc) in sc_data
-        delete!(data["branchdc"],"$branch_id")
-    end
-end
+#     for (branch_id,branch_dc) in sc_data
+#         delete!(data["branchdc"],"$branch_id")
+#     end
+# end
 
+# For P2P links
 function process_superconductor_links2!(data::Dict{String,Any})
     
     for (conv_id, conv_dc) in data["convdc"]        
@@ -45,13 +46,13 @@ function process_superconductor_links2!(data::Dict{String,Any})
             if branch_dc["sc"] == true
                 if conv_dc["busdc_i"] == branch_dc["tbusdc"] || conv_dc["busdc_i"] == branch_dc["fbusdc"]
                     conv_dc["busdc_i"] = branch_dc["fbusdc"]
-                    conv_dc["p_aux"] = branch_dc["p_aux"]/2
-                    conv_dc["q_aux"] = branch_dc["q_aux"]/2
+                    conv_dc["p_aux"] = branch_dc["p_aux"]/2 # Active power required for the auxiliaries (cooling stations)
+                    conv_dc["q_aux"] = branch_dc["q_aux"]/2 # Reactive power required for the auxiliaries (cooling stations)
                     conv_dc["sc"]    =  true
                     # Increase capacity of converters (not in pu as they are converted later)
                     conv_dc["Pacmax"] = 300
                     conv_dc["Pacmin"] = -300
-                    # conv_dc["Imax"] = 3 # Its calculated in the function process_additional_data
+                    # conv_dc["Imax"] = 3 # Its calculated in the function process_additional_data based on P and Q
                     
                 end                
             end
@@ -90,18 +91,18 @@ end
 # Creates sc_data dictionary with superconducting branches data
 # Merges sc_data with the grid data
 
-function add_sc_links_2!(data::Dict{String,Any},sc_links::Vector{String})
-    sc_data = Dict{String,Any}() # Empy dict to save sc data
-    for sc_link in sc_links
-        if haskey(data["branchdc"],sc_link)
-            sc_data[sc_link] = deepcopy(data["branchdc"][sc_link])
-            sc_data[sc_link]["length"] = 200 # All sc branches 200 km, to modify later
-            sc_data[sc_link]["p_aux"] = cooling_losses(sc_data[sc_link]["length"])[1]
-            sc_data[sc_link]["q_aux"] = cooling_losses(sc_data[sc_link]["length"])[2]
-        end
-    end
-    merge!(data["branchdc"],sc_data)
-end
+# function add_sc_links_2!(data::Dict{String,Any},sc_links::Vector{String})
+#     sc_data = Dict{String,Any}() # Empy dict to save sc data
+#     for sc_link in sc_links
+#         if haskey(data["branchdc"],sc_link)
+#             sc_data[sc_link] = deepcopy(data["branchdc"][sc_link])
+#             sc_data[sc_link]["length"] = 200 # All sc branches 200 km, to modify later
+#             sc_data[sc_link]["p_aux"] = cooling_losses(sc_data[sc_link]["length"])[1]
+#             sc_data[sc_link]["q_aux"] = cooling_losses(sc_data[sc_link]["length"])[2]
+#         end
+#     end
+#     merge!(data["branchdc"],sc_data)
+# end
 
 # Function modifying directly data dict
 function add_sc_links_3!(data::Dict{String,Any},sc_links::Vector{String})
@@ -132,6 +133,8 @@ function cooling_losses(length)
     push!(aux_power,q_aux) # Reactive power losses
     return aux_power # Vector with [p_aux,q_aux]
 end
+
+## Put these functions in a different file
 
 # Function to calculate losses in transmission/conversion
 function process_results!(result::Dict{String,Any})
