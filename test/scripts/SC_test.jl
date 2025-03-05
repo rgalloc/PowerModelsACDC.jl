77d4c895538960d1220b1b78e8df9c794ef43de8
Ricardo Gallo <ricardoandres.gallocaicedo@kuleuven.be>
2025-03-05 11:29:30 +0100

    update new sc function

--- test/scripts/SC_test.jl
+++ test/scripts/SC_test.jl
@@ -18,8 +18,8 @@ nl_solver = Ipopt.Optimizer
 #nl_solver = Gurobi.Optimizer
 
 # Define superconductor links
-#sc_links = ["1","2"] # Vector to state which dc branches are superconductor links
-sc_links = ["1","2","3"] # For meshed
+sc_links = ["1","2"] # Vector to state which dc branches are superconductor links
+#sc_links = ["1","2","3"] # For meshed
 #sc_links = [string(i) for i in 1:11]
 #sc_data = Dict{String,Any}() # Dict to save sc branches data
 
@@ -29,12 +29,13 @@ sc_data = _PMACDC.add_sc_links_3!(data,sc_links)
 
 #process_superconductor_links2!(data)
 #process_sc_meshed!(data)
+_PMACDC.process_superconductor_links_new!(data)
 
 s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true)
 
 _PMACDC.process_additional_data!(data)
 _PMACDC.process_additional_data!(data_original)
-result = _PMACDC.run_acdcopf(data,ACPPowerModel,nl_solver;setting = s)
+#result = _PMACDC.run_acdcopf(data,ACPPowerModel,nl_solver;setting = s)
 result_original = _PMACDC.run_acdcopf(data_original,ACPPowerModel,nl_solver;setting = s)
 result_sc = _PMACDC.run_acdcopf_superconducting(data,ACPPowerModel,nl_solver;setting = s)
 #result_2 = _PMACDC.run_acdcopf(data,ACPPowerModel,nl_solver;setting = s)
@@ -43,8 +44,20 @@ print("-------------------------------------------------------\n")
 print("                  Optimization Results                 \n")
 print("-------------------------------------------------------\n")
 print("\n Case 1: No SC Branches : ",result_original["objective"], "\n")
-print("\n Case 2: 1  SC Branch(es)   : ",result["objective"], "\n")
+#print("\n Case 2: 1  SC Branch(es)   : ",result["objective"], "\n")
 print("\n Case 2: 2  SC Branch(es)   : ",result_sc["objective"], "\n")
+
+# power flow in the dc lines
+## Original
+# 1 = 0.451
+# 2 = 0.405
+# 3 = 0.03
+
+## With 1 and 2 SC
+# 1 = 0.608
+# 2 = 0.432
+# 3 = 1.09
+
 
 # Saving results to JSON
 results_path = "/Users/rgallo/Library/CloudStorage/OneDrive-KULeuven/OPF_SC" 
