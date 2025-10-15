28d8a1737f1bab7a4fc5d8cd7306900218dbd76d
Ricardo Gallo <117919292+rgalloc@users.noreply.github.com>
2025-10-15 09:25:16 -0500

    Add SC_test.jl for superconductor link testing
    Initial setup for testing superconductor link functions.

---
+++ test/scripts/SC_test.jl
@@ -0,0 +1,9 @@
+###################################################################
+#####   Code to test new functions to add superconductor links #####
+###################################################################
+
+using PowerModels ; const _PM = PowerModels
+using PowerModelsACDC ; const _PMACDC = PowerModelsACDC
+using Gurobi
+using Ipopt
+using JSON
