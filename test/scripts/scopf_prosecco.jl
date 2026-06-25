a9a24667ebd566e4a4bf683459c306a6e295ab59
Ricardo Gallo <ricardoandres.gallocaicedo@kuleuven.be>
2026-06-25 11:49:48 +0200

    testing scopf functions

--- test/scripts/scopf_prosecco.jl
+++ test/scripts/scopf_prosecco.jl
@@ -4,17 +4,17 @@ import Ipopt
 import Plots
 
 #solver and settings
-nlsolver = optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6, "print_level" => 4)
-# s = Dict("conv_losses_mp" => true)
+nlsolver = optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6, "print_level" => 3)
+s = Dict("conv_losses_mp" => true)
 
-# # data parsing
-# data = PowerModels.parse_file("test/data/prosecco_base.m")
-# process_additional_data!(data)
+ # data parsing
+data_single = PowerModels.parse_file("test/data/prosecco_base.m")
+process_additional_data!(data_single)
 
-# result = solve_acdcopf(data, PowerModels.ACPPowerModel, nlsolver; setting=s)
+result_single = solve_acdcopf(data_single, PowerModels.ACPPowerModel, nlsolver; setting=s)
 
-# # LF = [0.75, 1, 1.25]
-# # CF = [0.25, 0.5, 0.75, 1]
+ # LF = [0.75, 1, 1.25]
+ # CF = [0.25, 0.5, 0.75, 1]
 # LF = [1.0 0.7 0.75 0.78 0.85]
 # CF = [1.0 0.7 0.75 0.78 0.85]
 
@@ -75,75 +75,145 @@ number_of_contingencies = length(data["contingencies"])
 data_all = create_scopf_data(data, number_of_hours, g_series, l_series)
 
 # N-1 
-for idx = 1:19 # idx [1:10] for converter outage, idx [11:19] for dc branch outage
+for idx = 1:6
     nw = idx + 1
-    if idx <= 11
-        data_all["nw"]["$nw"]["convdc"]["$idx"]["status"] = 0
-    else
-        data_all["nw"]["$nw"]["branchdc"]["$(idx-10)"]["status"] = 0
-    end
-    # data_all["nw"]["$nw"]["convdc"]["$idx"]["status"] = 0
-    for (b, busdc) in  data_all["nw"]["$nw"]["busdc"]
-        busdc["Vdcmax"] = 1.8
-        busdc["Vdcmin"] = 0.2
-    end
+    data_all["nw"]["$nw"]["convdc"]["$idx"]["status"] = 0
+    # for (b, busdc) in  data_all["nw"]["$nw"]["busdc"]
+    #     busdc["Vdcmax"] = 2
+    #     busdc["Vdcmin"] = 0.2
+    # end
 end
+
+# for idx = 4:9
+#     nw = idx + 8
+#     data_all["nw"]["$nw"]["branchdc"]["$idx"]["status"] = 0
+# end
+
+# for nw in 1:number_of_hours * number_of_contingencies
+#     for (b, busdc) in  data_all["nw"]["$nw"]["busdc"]
+#         busdc["Vdcmax"] = 2
+#         busdc["Vdcmin"] = 0.2
+#     end
+# end
 
 
 # Solve OPF
 result = solve_scopf(data_all, PowerModels.ACPPowerModel, nlsolver; multinetwork=true, setting=s)
 
-############ PRINT droop coefficient for converters ###################
-k_droop = zeros(length(result["solution"]["nw"]["1"]["convdc"]))
-for (c, conv) in result["solution"]["nw"]["1"]["convdc"]
-    k_droop[parse(Int, c)] = conv["k_droop"]
-end
-
-
-########## PRINT Converter dc side & generator setpoints for all hours and contingencies
-
+############ Processing the results ###################
+k_droop = zeros(number_of_hours, length(result["solution"]["nw"]["1"]["convdc"]))
 pconv = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["convdc"]))
 qconv = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["convdc"]))
 pg = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["gen"]))
 qg = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["gen"]))
-pd = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["load"]))
+# pd = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["load"]))
+branch_flows = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["branchdc"]))
+busdc_voltages = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["busdc"]))
+
+baseMVA = data_all["nw"]["1"]["baseMVA"]
+
+for (c,conv) in result["solution"]["nw"]["1"]["convdc"]
+    k_droop[1, parse(Int, c)] = conv["k_droop"]
+end
 
 for (n, network) in result["solution"]["nw"]
     for (g, gen) in network["gen"]
-        pg[parse(Int, n), parse(Int, g)] = gen["pg"] * data_all["nw"]["1"]["baseMVA"]
-        qg[parse(Int, n), parse(Int, g)] = gen["qg"] * data_all["nw"]["1"]["baseMVA"]
+        pg[parse(Int, n), parse(Int, g)] = gen["pg"]*baseMVA
+        qg[parse(Int, n), parse(Int, g)] = gen["qg"]*baseMVA
     end
     for (c, conv) in network["convdc"]
-        pconv[parse(Int, n), parse(Int, c)] = conv["pdc"] * data_all["nw"]["1"]["baseMVA"]
-        qconv[parse(Int, n), parse(Int, c)] = conv["qconv"] * data_all["nw"]["1"]["baseMVA"]
+        pconv[parse(Int, n), parse(Int, c)] = conv["pdc"]*baseMVA
+        qconv[parse(Int, n), parse(Int, c)] = conv["qconv"]*baseMVA
     end
-    for (l, load) in network["load"]
-        pd[parse(Int, n), parse(Int, l)] = load["pflex"] * data_all["nw"]["1"]["baseMVA"]
-    end
-end
-
-total_load = sum(pd, dims=2)
-total_gen = sum(pg, dims=2)
-losses = total_gen - total_load
-
-wind_pmax = zeros(number_of_hours * number_of_contingencies, 2)
-for (n, network) in data_all["nw"]
-    for (g, gen) in network["gen"]
-        if g in ["4", "5"]
-            wind_pmax[parse(Int, n), parse(Int, g)-3] = gen["pmax"] * data_all["nw"]["1"]["baseMVA"]
-        end
-    end
-end
-wind_pmax_total = sum(wind_pmax, dims=2)
-
-diff = wind_pmax_total - total_gen
-
-branch_flows = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["branchdc"]))
-for (n, network) in result["solution"]["nw"]
+    # for (l, load) in network["load"]
+    #     pd[parse(Int, n), parse(Int, l)] = load["pflex"]
+    # end
     for (bdc, branchdc) in network["branchdc"]
-        branch_flows[parse(Int, n), parse(Int, bdc)] = max(abs(branchdc["pf"]), abs(branchdc["pt"])) * data_all["nw"]["1"]["baseMVA"]
+        branch_flows[parse(Int, n), parse(Int, bdc)] = max(abs(branchdc["pf"]), abs(branchdc["pt"]))*baseMVA
+    end
+    for (bdc, busdc) in network["busdc"]
+        busdc_voltages[parse(Int, n), parse(Int, bdc)] = busdc["vm"]
     end
 end
+
+### Computing voltage differences
+
+vdc_min = 0.9
+vdc_max = 1.1
+
+margin_low = busdc_voltages .- vdc_min
+margin_high = vdc_max .- busdc_voltages
+
+dc_voltage_diff = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["branchdc"]))
+
+for (b,branchdc) in data_all["nw"]["1"]["branchdc"]
+    b_idx = parse(Int, b)
+
+    fbus = branchdc["fbusdc"]
+    tbus = branchdc["tbusdc"]
+
+    for n in 1:number_of_hours * number_of_contingencies
+        u_f = busdc_voltages[n, fbus]
+        u_t = busdc_voltages[n, tbus]
+
+        dc_voltage_diff[n, b_idx] = abs(u_f - u_t)
+    end
+end
+
+droop_v_diff = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["convdc"]))
+droop_p_diff = zeros(number_of_hours * number_of_contingencies, length(result["solution"]["nw"]["1"]["convdc"]))
+
+
+for (n, network) in result["solution"]["nw"]
+    n_idx = parse(Int, n)
+
+    # Reference network of the corresponding hour
+    ref_idx = div(n_idx - 1, number_of_contingencies) * number_of_contingencies + 1
+    ref_key = string(ref_idx)
+
+    for (c, conv) in network["convdc"]
+        c_idx = parse(Int, c)
+
+        busdc_id = string(data_all["nw"][ref_key]["convdc"][c]["busdc_i"])
+
+        u_ref = result["solution"]["nw"][ref_key]["busdc"][busdc_id]["vm"]
+        u_con = result["solution"]["nw"][n]["busdc"][busdc_id]["vm"]
+
+        p_ref = result["solution"]["nw"][ref_key]["convdc"][c]["pdc"]
+        p_con = result["solution"]["nw"][n]["convdc"][c]["pdc"]
+
+        droop_v_diff[n_idx, c_idx] = u_con - u_ref
+        droop_p_diff[n_idx, c_idx] = (p_con - p_ref) * data_all["nw"]["1"]["baseMVA"]
+    end
+end
+
+droop_p_diff_pu = droop_p_diff ./ data_all["nw"]["1"]["baseMVA"]
+k_calc = droop_p_diff_pu ./ droop_v_diff
+
+
+
+
+
+
+
+# total_load = sum(pd, dims=2)
+# total_gen = sum(pg, dims=2)
+# losses = total_gen - total_load
+
+# wind_pmax = zeros(number_of_hours * number_of_contingencies, 2)
+# for (n, network) in data_all["nw"]
+#     for (g, gen) in network["gen"]
+#         if g in ["4", "5"]
+#             wind_pmax[parse(Int, n), parse(Int, g)-3] = gen["pmax"] * data_all["nw"]["1"]["baseMVA"]
+#         end
+#     end
+# end
+# wind_pmax_total = sum(wind_pmax, dims=2)
+
+# diff = wind_pmax_total - total_gen
+
+
+
 
 # for (n, network) in result["solution"]["nw"]
 #     if parse(Int, n) < data_all["number_of_contingencies"]
@@ -169,3 +239,18 @@ function scale_load_wind(data, LF, CF)
     end
     return data_run
 end
+
+# Postprocessing of results
+
+# function dc_voltage_margins(busdc_voltages)
+#     vdc_min = 0.9
+#     vdc_max = 1.1
+
+#     margin_low = busdc_voltages .- vdc_min
+#     margin_high = vdc_max .- busdc_voltages
+
+#     return margin_low, margin_high
+# end
+
+# function dc_branch_diff(busdc_voltages)
+# end
